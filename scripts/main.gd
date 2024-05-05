42b17d5f037197c35ff628409cc3af4838a01741
loethor <lrb2392@gmail.com>
2024-05-05 18:43:24 +0200

    Adding brush size variation with + and -

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,13 +1,17 @@
 extends Node2D
 
-@export var brush_size: int = 1
+# brush stuff
+@export var brush_size: int = MIN_BRUSH_SIZE : set = set_brush
+const MIN_BRUSH_SIZE := 1
+const MAX_BRUSH_SIZE := 5
+func set_brush(value:int) -> void:
+	brush_size = clamp(value, MIN_BRUSH_SIZE, MAX_BRUSH_SIZE)
+
 @onready var sand_label: Label = $GUI/VBoxContainer/SandLabel
 @onready var water_label: Label = $GUI/VBoxContainer/WaterLabel
 @onready var fps: Label = $GUI/FPS
 
 var n_steps := 0
-
-
 
 enum MATERIALS {AIR, BEDROCK, SAND, WATER}
 const MATERIAL_TO_ATLAS_COORD:Dictionary = {
@@ -17,11 +21,15 @@ const MATERIAL_TO_ATLAS_COORD:Dictionary = {
 }
 const MAIN_LAYER := 0
 
+
 var material_in_hand: MATERIALS = MATERIALS.BEDROCK
 
 @onready var tile_map: TileMap = $TileMap
 
+
 func _input(event: InputEvent) -> void:
+
+	# changing materials with 1,2,3...
 	if event.is_action_pressed("sand"):
 		material_in_hand = MATERIALS.SAND
 	if event.is_action_pressed("water"):
@@ -32,9 +40,17 @@ func _input(event: InputEvent) -> void:
 func _process(delta: float) -> void:
 	fps.text = "%s" % Engine.get_frames_per_second()
 
-	if Input.is_action_pressed("drop"):
+	if Input.is_action_just_pressed("increase_brush"):
+		brush_size += 1
+	if Input.is_action_just_pressed("decrease_brush"):
+		brush_size -= 1
+
+	# placing pixels in a square of brush_size size around mouse position
+	if Input.is_action_pressed("place"):
 		var mouse_pos = Vector2i(get_global_mouse_position())
-		tile_map.set_cell(MAIN_LAYER,mouse_pos,0,MATERIAL_TO_ATLAS_COORD[material_in_hand])
+		for i in range(mouse_pos.x - brush_size + 1, mouse_pos.x + brush_size):
+			for j in range(mouse_pos.y - brush_size + 1, mouse_pos.y + brush_size):
+				tile_map.set_cell(MAIN_LAYER,Vector2i(i,j),0,MATERIAL_TO_ATLAS_COORD[material_in_hand])
 
 func loop_tile_set() -> void:
 	n_steps += 1
@@ -67,7 +83,6 @@ func set_cells_next_generation(next_generation_cells: Array[Vector2i], cell_mate
 
 func process_cells(array_of_cells:Array[Vector2i], cell_material:MATERIALS) -> Array[Vector2i]:
 	var next_generation_cells: Array[Vector2i] = []
-
 
 	match cell_material:
 		MATERIALS.SAND:
@@ -140,74 +155,17 @@ func get_next_generation_water(array_of_cells:Array[Vector2i]) -> Array[Vector2i
 			pass
 	return next_generation_cells
 
-func process_sand_cells(sand_cells:Array[Vector2i]) -> Array[Vector2i]:
-	var future_sand_cells:Array[Vector2i] = []
-	for sand_cell in sand_cells:
-		var is_down_empty = is_cell_empty_at(Vector2i(sand_cell.x, sand_cell.y + 1))
-		#var is_left_empty = is_cell_empty_at(Vector2i(cell.x - 1, cell.y))
-		#var is_right_empty = is_cell_empty_at(Vector2i(cell.x + 1, cell.y))
-		var is_down_left_empty = is_cell_empty_at(Vector2i(sand_cell.x - 1, sand_cell.y + 1))
-		var is_down_right_empty = is_cell_empty_at(Vector2i(sand_cell.x + 1, sand_cell.y + 1))
-
-		if is_down_empty:
-			tile_map.set_cell(MAIN_LAYER, sand_cell, -1)
-			future_sand_cells.append(Vector2i(sand_cell.x, sand_cell.y + 1))
-		elif is_down_left_empty and is_down_right_empty:
-			tile_map.set_cell(MAIN_LAYER, sand_cell, -1)
-			if randi() % 2 == 1:
-				future_sand_cells.append(Vector2i(sand_cell.x - 1, sand_cell.y + 1))
-			else:
-				future_sand_cells.append(Vector2i(sand_cell.x + 1, sand_cell.y + 1))
-		elif is_down_left_empty:
-			tile_map.set_cell(MAIN_LAYER, sand_cell, -1)
-			future_sand_cells.append(Vector2i(sand_cell.x - 1, sand_cell.y + 1))
-		elif is_down_right_empty:
-			tile_map.set_cell(MAIN_LAYER, sand_cell, -1)
-			future_sand_cells.append(Vector2i(sand_cell.x + 1, sand_cell.y + 1))
-		else:
-			pass
-
-	return future_sand_cells
-
+# for a cell to be available needs to be empty and also not already
+# taken for next generation
 func is_position_available(at_position: Vector2i, next_generation_cells: Array[Vector2i]) -> bool:
 	return is_cell_empty_at(at_position) and at_position not in next_generation_cells
 
-func process_water_cells(water_cells:Array[Vector2i]) -> Array[Vector2i]:
-	var future_water_cells:Array[Vector2i] = []
 
-	for cell in water_cells:
-		var down_cell := Vector2i(cell.x, cell.y + 1)
-		var is_down_empty = is_cell_empty_at(down_cell)
-		var left_cell := Vector2i(cell.x - 1, cell.y)
-		var is_left_empty = is_cell_empty_at(left_cell)
-		var right_cell := Vector2i(cell.x + 1, cell.y)
-		var is_right_empty = is_cell_empty_at(right_cell)
-
-		if is_down_empty and down_cell not in future_water_cells:
-			tile_map.set_cell(MAIN_LAYER, cell, -1)
-			future_water_cells.append(Vector2i(cell.x, cell.y + 1))
-		elif is_left_empty and is_right_empty:
-			var choices = [left_cell, right_cell]
-			var rand_choice = choices[randi() % len(choices)]
-			if rand_choice not in future_water_cells:
-				future_water_cells.append(rand_choice)
-				tile_map.set_cell(MAIN_LAYER, cell, -1)
-		elif is_left_empty and left_cell not in future_water_cells:
-			tile_map.set_cell(MAIN_LAYER, cell, -1)
-			future_water_cells.append(left_cell)
-		elif is_right_empty and right_cell not in future_water_cells:
-			tile_map.set_cell(MAIN_LAYER, cell, -1)
-			future_water_cells.append(right_cell)
-		else:
-			pass
-
-	return future_water_cells
-
-
+# checks if the id of the cell at cell_position is -1 (empty)
 func is_cell_empty_at(cell_position: Vector2i) -> bool :
 	return tile_map.get_cell_source_id(MAIN_LAYER, cell_position) == -1
 
+# used for simulation speed
 func _on_timer_timeout() -> void:
 	loop_tile_set()
-
 	$Timer.start()
